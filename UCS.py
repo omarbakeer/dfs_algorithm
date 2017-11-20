e65934c298d438fa50d610e176add667acf4925a
Omar Bakier <omarbakeer@gmail.com>
2017-11-20 15:24:10 +0200

    UCS documentation and modulatrity
    Documenting the algorithm and inhancing it's modularity by isloation romania's initialization

--- UCS.py
+++ UCS.py
@@ -1,53 +1,36 @@
 from graph import Graph 
+import romania as RO
 
-cities = "Arad Bucharest Craiova Dobreta Eforie Fagaras Giurgiu Hirsowa Lasi Lugoj Mehadia Neamt Oradea Pitesti Rimnicu-Vilcea Sibiu Timisoara Urziceni Vaslui Zerind".split()
-romania = Graph(20,cities)
-romania.add_edge('Sibiu', 'Arad', 140)
-romania.add_edge('Zerind', 'Arad', 75)
-romania.add_edge('Timisoara', 'Arad', 118)
-romania.add_edge('Oradea', 'Zerind', 71)
-romania.add_edge('Sibiu', 'Oradea', 151)
-romania.add_edge('Lugoj', 'Timisoara', 111)
-romania.add_edge('Mehadia', 'Lugoj', 70)
-romania.add_edge('Dobreta', 'Mehadia', 75)
-romania.add_edge('Craiova', 'Dobreta', 120)
-romania.add_edge('Rimnicu-Vilcea', 'Craiova', 146)
-romania.add_edge('Pitesti', 'Craiova', 138)
-romania.add_edge('Sibiu', 'Rimnicu-Vilcea', 80)
-romania.add_edge('Pitesti', 'Rimnicu-Vilcea', 97)
-romania.add_edge('Bucharest', 'Pitesti', 101)
-romania.add_edge('Fagaras', 'Sibiu', 99)
-romania.add_edge('Bucharest', 'Fagaras', 211)
-romania.add_edge('Giurgiu', 'Bucharest', 90)
-romania.add_edge('Urziceni', 'Bucharest', 85)
-romania.add_edge('Hirsowa', 'Urziceni', 98)
-romania.add_edge('Vaslui', 'Urziceni', 142)
-romania.add_edge('Eforie', 'Hirsowa', 86)
-romania.add_edge('Lasi', 'Vaslui', 92)
-romania.add_edge('Neamt', 'Lasi', 87)
-
-def ucs(start='Arad',end='Bucharest',graph=romania):
-	
+def ucs(graph, start='Arad', end='Bucharest'):
+	''' A queue for the successor nodes to be explored with the format
+	of [(cost, node's name, [path to this node])]				'''
 	myQueue = [(0,start,[])] 
-	visited = {}
+	# A dictionary with the visited nodes and their costs/values
+	visited = {} 
 
 	while myQueue:
-
+		# Visit the node of lowest cost (first node)
 		cost, node, path = myQueue.pop(0)
-
+		# Skip if the node you're visiting is already visited with a lower cost
 		if node in visited and visited[node] < cost:
 			continue
-
-		path = path+ [node]
+		# Set the new path
+		path = path + [node]
+		# Return path if you reached the destination
 		if node == end:
 			return path
-
+		# Get the adjacents and loop through them
 		adjacents = graph.get_adjacents(node)
 		for adj in adjacents:
+			''' Condition to prevent the graph to go inside loop 
+			if you did Arad >> Sibiu you can't do Sibiu >> Arad '''
 			if adj not in visited:
 				myQueue.append((cost+graph.get_weight(node,adj),adj,path))
+
+		# Mark this node as visited with it's new cost
 		visited[node] = cost
+		# Order the successors ascending
 		myQueue.sort()
-	# print(myQueue)
-	# print(visited)
-print(ucs())
+
+romania = RO.romania_init()	
+print(ucs(graph=romania))
